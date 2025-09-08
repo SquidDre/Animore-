518e8fa0cf93e2b7196b611b4c9e9783805d9019
DeAndre Bailey <deandrebailey@outlook.com>
2025-09-08 15:08:11 -0400

    Updating Explore Page and Adding Lib necessary for mongodb search bar

--- ani-app/app/explore/page.tsx
+++ ani-app/app/explore/page.tsx
@@ -1,12 +1,38 @@
+"use client"; // 👈 Required if you're in Next.js App Router
+
+import Image from "next/image";
+import { useRouter } from "next/navigation"; // 👈 Next.js router hook
+import Search from "../../public/search.svg";
+import "../globals.css";
 export default function ExplorePage() {
+  const router = useRouter();
+
   return (
     <div>
-      <h1 className="mb-8 m-4 text-3xl font-bold">
+      <div className="flex justify-between gap-4 m-4">
+        <h1 className="m-4 mt-4 text-4xl font-bold flex">
             ANIMORE
             <span className="animate-blink text-amber-300">+</span>
-      </h1>
-      <div className="flex items-center justify-center min-h-screen">
-        <h1 className="text-4xl font-bold">Welcome to Explore!</h1>
+        </h1>
+        <div className="flex gap-4 ml-auto">
+          <button
+            onClick={() => router.push("/signup")}
+            className="flex-wrap-reverse outline-2 px-6 py-3 my-3 bg-black text-white hover:bg-white hover:text-black transition font-bold"
+          >
+            SIGNUP
+          </button>
+
+          <button
+            onClick={() => router.push("/login")}
+            className="flex-wrap-reverse outline-2 px-6 py-3 my-3 bg-black text-white hover:bg-white hover:text-black transition font-bold"
+          >
+            LOGIN
+          </button>
+        </div>
+          
+      </div>
+      <div className="flex justify-center min-h-screen">
+        <h1 className="text-6xl font-bold mt-8">EXPLORE+</h1>
       </div>
     </div>
   );

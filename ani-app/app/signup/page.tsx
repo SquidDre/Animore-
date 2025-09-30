550e124d00d81611ab8a36f646ff0411b3e36a8f
DeAndre Bailey <deandrebailey@outlook.com>
2025-09-30 15:30:47 -0400

    prisma set up

---
+++ ani-app/app/signup/page.tsx
@@ -0,0 +1,14 @@
+"use client"; // 👈 Required if you're in Next.js App Router
+
+import Image from "next/image";
+import { useRouter } from "next/navigation"; // Next.js router hook
+import SearchBar from "../../components/SearchBar"
+import "../globals.css";
+export default function SignupPage() {
+  const router = useRouter();
+
+  return (
+    
+    
+  );
+}
